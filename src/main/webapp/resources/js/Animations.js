22ad26875a4eb392fcbf3385a5f3701537a5d67c
Alfonso Terrones Moreno <Alfonso Terrones Moreno>
2017-05-28 23:30:31 +0200

    css,js

--- src/main/webapp/resources/js/Animations.js
+++ src/main/webapp/resources/js/Animations.js
@@ -1,32 +1,56 @@
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-$("span").hide();
-$("#panelPrincipal").hide();
-$("#escudo").hide();
-  setInterval(descargar, 1000);
+$("#probemos").click(function(){
+   alert("funciona"); 
+});
 
-  function descargar() {
-  $("#cargando").hide();
-  $("#panelPrincipal").fadeIn();
-  $("#escudo").fadeIn();
-    setInterval(descargariconos, 1000);
-  $("#div2").fadeIn("slow");
+    window.onload = funcion_primera;
+    function funcion_primera() {
+        alert("Se ha lanzado la PRIMERA función");
+    }
+    window.onload = funcion_primera;
 
-  }
 
-  function descargariconos(){
-    $("span").fadeIn(2000);
-  }
+    $("span").hide();
+    $("#panelPrincipal").hide();
+    $("#escudo").hide();
+    $("#subFooter").hide();
+    setInterval(descargar, 1000);
+
+    function descargar() {
+        $("#cargando").hide();
+        $("#panelPrincipal").fadeIn();
+        $("#escudo").fadeIn();
+        setInterval(descargariconos, 1000);
+        $("#div2").fadeIn("slow");
+    }
+
+    $('#botonMenu1').click(function () {
+        cambiarVentana();
+        return false;
+    });
+
+    function cambiarVentana() {
+        $("span").fadeOut("slow");;
+        $("#panelPrincipal").fadeOut("slow");;
+        $("#escudo").fadeOut("slow");;
+        $("#subFooter").fadeOut("slow");;
+        window.location("OrdenadorYAlumno.xhtml");
+    }
+    function descargariconos() {
+        $("span").fadeIn(1500);
+        $("#subFooter").fadeIn(1500)
+    }
 
 });
 
-var app = angular.module('appWeb',[]);
-var botonPulsado=0;
+var app = angular.module('appWeb', []);
+var botonPulsado = 0;
 
 
-  app.controller('controladorPractica',function($scope){
+app.controller('controladorPractica', function ($scope) {
 
 
-  });
+});
